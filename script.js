a04002124141de4d3a0d3c94a82c8789316dab81
Sattarbi <sattarbisheik3@gmail.com>
2026-06-18 20:13:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,56 @@
+const roles = [
+    "Computer Science Student",
+    "Java Developer",
+    "Frontend Developer",
+    "Problem Solver"
+];
+
+let roleIndex = 0;
+let charIndex = 0;
+
+const typing = document.getElementById("typing");
+
+function typeEffect() {
+
+    if (charIndex < roles[roleIndex].length) {
+
+        typing.textContent += roles[roleIndex].charAt(charIndex);
+
+        charIndex++;
+
+        setTimeout(typeEffect, 100);
+
+    } else {
+
+        setTimeout(eraseEffect, 1500);
+
+    }
+
+}
+
+function eraseEffect() {
+
+    if (charIndex > 0) {
+
+        typing.textContent =
+            roles[roleIndex].substring(0, charIndex - 1);
+
+        charIndex--;
+
+        setTimeout(eraseEffect, 50);
+
+    } else {
+
+        roleIndex++;
+
+        if (roleIndex >= roles.length) {
+            roleIndex = 0;
+        }
+
+        setTimeout(typeEffect, 300);
+
+    }
+
+}
+
+document.addEventListener("DOMContentLoaded", typeEffect);
